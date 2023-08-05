87c151ae01abdf4da778ee2d9a1398d27bb39d28
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-08-05 07:47:16 +0600

    added documentation

--- lib/src/models/pin_input_decoration.dart
+++ lib/src/models/pin_input_decoration.dart
@@ -1,17 +1,56 @@
 import 'package:flutter/material.dart';
 
 class PinInputTheme {
-  final EdgeInsetsGeometry? padding;
-  final EdgeInsetsGeometry? margin;
-  final Decoration? decoration;
-  final InputDecoration? inputDecoration;
+  /// width of each [Pinput] field
+  final double? width;
+
+  /// height of each [Pinput] field
+  final double? height;
+
+  /// The style to use for PinPut
+  /// If null, defaults to the `subhead` text style from the current [Theme].
   final TextStyle? textStyle;
 
+  /// Empty space to surround the [Pinput] field container.
+  final EdgeInsetsGeometry? margin;
+
+  /// Empty space to inscribe the [Pinput] field container.
+  /// For example space between border and text
+  final EdgeInsetsGeometry? padding;
+
+  /// Additional constraints to apply to the each field container.
+  /// properties
+  /// ```dart
+  ///  this.minWidth = 0.0,
+  ///  this.maxWidth = double.infinity,
+  ///  this.minHeight = 0.0,
+  ///  this.maxHeight = double.infinity,
+  ///  ```
+  final BoxConstraints? constraints;
+
+  ///  Box decoration of following properties of Pin item
+  ///  You can customize every pixel with it
+  ///  properties are being animated implicitly when value changes
+  ///  ```dart
+  ///  this.color,
+  ///  this.image,
+  ///  this.border,
+  ///  this.borderRadius,
+  ///  this.boxShadow,
+  ///  this.gradient,
+  ///  this.backgroundBlendMode,
+  ///  this.shape = BoxShape.rectangle,
+  ///  ```
+  /// The decoration of each [Pinput] submitted field
+  final BoxDecoration? decoration;
+  
   PinInputTheme({
-    this.padding,
-    this.margin,
-    this.decoration,
-    this.inputDecoration,
+    this.width,
+    this.height,
     this.textStyle,
+    this.margin,
+    this.padding,
+    this.constraints,
+    this.decoration,
   });
 }
